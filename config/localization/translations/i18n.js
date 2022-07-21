d0bf7837271d0adba829942acffc3aca31ffd556
rafael.ramirez <rafa.rami12.10@gmail.com>
2022-07-21 01:07:05 -0500

    Feat:folder was created, where the file for language translation is located

---
+++ config/localization/translations/i18n.js
@@ -0,0 +1,31 @@
+/*import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+
+i18n
+   .use(initReactI18next) // passes i18n down to react-i18next
+   .init({
+    resources:{
+        en: {
+            traslation:{
+                info: "I like listening to music, reading, watching movies and series,When I finish the degree, I hope to be able to work as a developer and continue learning more about the degree",
+                
+            },
+        },
+        es: {
+            traslation:{
+                info: "A mi me gusta escuchar musica, leer, ver peliculas y series, cuando termine la carrera, espero poder trabajar como desarrolladora y seguir aprendiendo mas cosas sobre la carrera",
+            },
+        },
+    },
+    fallbackLng: "en",
+    interpoltion: {
+        escapeValue: false,
+    },
+});
+
+export default i18n;
+
+
+*/
+
